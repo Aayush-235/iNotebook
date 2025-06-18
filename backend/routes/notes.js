b657ff629aceb688dbb0dbf6a06891e9bd62e88d
Savaliya Aayush Rameshbhai <aayushsavaliya235@gmail.com>
2025-06-18 18:50:30 +0530

    Update Notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ const Notes = require("../models/Notes")
 const { body, validationResult } = require('express-validator');
 
 //ROUTE--1
-//Get all the notes using : GET "/api/auth/fetchallnotes"
+//Get all the notes using : GET "/api/notes/fetchallnotes"
 
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
@@ -17,7 +17,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
 })
 //ROUTE--2
-//Add a new notes using : POST "/api/auth/addnotes"
+//Add a new notes using : POST "/api/notes/addnotes"
 
 router.post('/addnotes', fetchuser, [
 
@@ -51,5 +51,50 @@ router.post('/addnotes', fetchuser, [
 
 
     })
+
+
+//ROUTE--3
+//Update existing notes using : POST "/api/notes/updatenotes"
+
+router.put('/updatenotes/:id', fetchuser, async (req, res) => {
+
+    try {
+        // console.log("at try block")
+        const { title, description, tag } = req.body
+
+        //Create newnotes object
+
+        const newnotes = {}
+        if (title) { newnotes.title = title }
+        if (description) { newnotes.description = description }
+        if (tag) { newnotes.tag = tag }
+        
+        //Find the note to be updated and update it
+
+        let note = await Notes.findById(req.params.id)
+        if (!note) {
+            return res.status(404).send("Note not found")
+        }
+        if (note.user.toString() != req.user) {
+            return res.status(401).send("Not authorized")
+
+        }
+
+        // console.log("here")
+
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newnotes }, { new: true })
+        // note = await Notes.findOneAndUpdate({_id : req.params.id}, { $set: newnotes }, { new: true })
+        res.json({ note })
+
+
+    }
+
+    catch (err) {
+        // console.log("at catch block")
+        res.status(500).send("Internal Server Error")
+    }
+
+
+})
 
 module.exports = router
