6c07efb4c8d0fc06cbc95664487052c15265870e
Savaliya Aayush Rameshbhai <aayushsavaliya235@gmail.com>
2025-06-18 13:47:03 +0530

    Encrypt password and give jwt_token to user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/User")
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "mysecretkey";
 
 // . means = current dir
 // ..means = parent dir (one level up)
@@ -26,13 +30,22 @@ router.post('/createuser', [
             if (user) {
                 return res.status(400).json({ error: "Email already exists" })
             }
+
+            const salt = await bcrypt.genSalt(10);
+            const setPass = await bcrypt.hash(req.body.password, salt);
+
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
+                password: setPass
             })
 
-            res.json(user)
+            const data = {
+                user : user.id,
+            }
+            const authToken = jwt.sign(data, JWT_SECRET)
+            // console.log(authToken) to show the jwt_token in console
+            res.json({authToken})
         }
         catch (err) {
             res.status(500).send("Some error occurred")
