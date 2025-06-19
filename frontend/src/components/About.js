cfdcbc2489be91fddbbb994cecc9aba77dd3f869
Savaliya Aayush Rameshbhai <aayushsavaliya235@gmail.com>
2025-06-19 14:44:41 +0530

    useLocation

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,16 +2,10 @@ import React, { useContext, useEffect } from 'react'
 import noteContext from '../context/notes/noteContext'
 
 export default function About() {
-  const a = useContext(noteContext)
-  
-  useEffect(()=>{
-    a.update()
-    // eslint-disable-next-line
-  },[])
 
   return (
     <div>
-      this is about {a.state.name} and his class is {a.state.class} and his age is {a.state.age}
+      this is about page
     </div>
   )
 }
--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -2,24 +2,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Aayush",
-        "class": "5B",
-        "age": "18"
-    }
-    const [state, setState] = useState(s1)
-
-    const update = () => {
-        setTimeout(() => {
-            setState ({
-                "name": "Nikhil",
-                "class": "10B",
-                "age": "28"
-            })
-        }, 3000);
-    }
+    
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{}}>
             {
                 props.children
             }
